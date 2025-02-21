9e9e05d3ef728da21ef768b20870629ef3c9085b
Itsarun01 <hey.arunkumar05@gmail.com>
2025-02-21 17:02:58 +0530

    setup app, add pages and layout file, setup basic routing..

--- FrontEnd/src/App.jsx
+++ FrontEnd/src/App.jsx
@@ -1,11 +1,46 @@
 import "./App.css";
+import {createBrowserRouter, RouterProvider} from "react-router-dom";
+import AppLayout from "./components/Layout/AppLayout";
 
-function App() {
-  return (
-    <>
-     UI By Arun Branch testing
-    </>
-  );
-}
+import ErrorPage from "./components/Pages/ErrorPage";
+import Home from "./components/Pages/Home";
+import About from "./components/Pages/About";
+import Join from "./components/Pages/Join";
+import Contact from "./components/Pages/Contact";
+import Report from "./components/Pages/Report";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <AppLayout />,
+    errorElement: <ErrorPage />,
+    children: [
+      {
+        path: "/",
+        element: <Home />,
+      },
+      {
+        path: "/about",
+        element: <About />,
+      },
+      {
+        path: "/report",
+        element: <Report />,
+      },
+      {
+        path: "/join",
+        element: <Join />,
+      },
+      {
+        path: "/",
+        element: <Contact />,
+      },
+    ],
+  },
+]);
+
+const App = () => {
+  return <RouterProvider router={router} />;
+};
 
 export default App;
